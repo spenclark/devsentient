33c3ddd985d531e5e6ea13028bcdb7f0f8d6e64c
Spencer <spenclark@hotmail.com>
2021-10-18 17:53:25 -0700

    bottom styling !

--- app/src/Components/LandingHook.jsx
+++ app/src/Components/LandingHook.jsx
@@ -5,35 +5,35 @@ import React from "react"
 
 function LandingHook({props}) {
     return (
-        <Grid container item xs={12} style={{display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column-reverse", padding: "12px", margin: "auto", margin: "80px 0"}}>
-            <Grid item container xs={10} style={{border: "1px dashed #3E0E40", background: "#eeeeee", padding: "40px", borderRadius: "5px"}}>
+        <Grid container item xs={12} style={{display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column-reverse", padding: "12px", margin: "80px auto"}}>
+            <Grid item container xs={10} style={{border: "2px dashed #EEEEEE", background: "white", padding: "40px", borderRadius: "5px"}}>
                 <Grid item xs={12} style={{textAlign:"center", fontWeight: "600", fontSize: "24px", color: "#3E0E40"}}>
                     {props.title}
                 </Grid>
-                <Grid item xs={12} style={{textAlign:"center", color: "#343434"}}>
+                <Grid item xs={12} style={{textAlign:"center", color: "#3E0E40"}}>
                     {props.subTitle}
                 </Grid>
-                <Grid item container xs={12} style={{display:"flex", justifyContent: "center"}}>
-                    <Grid item xs={12} sm={3} style={{textAlign: "center", padding: "10px 18px", margin: "10px",  borderRadius: "3px", background: "#dba10d", display: "flex", justifyContent: 'center', alignItems: "center"}}>
-                        <a href={props.LeftButton.to} style={{textDecoration: "none", fontWeight: "600", color: "white"}}>
-                            {props.LeftButton.title}
-                        </a>
-                    </Grid>
-                    <Grid item xs={12} sm={3} style={{textAlign: "center", padding: "10px 18px", margin: "10px", border: "#dba10d 2px solid", borderRadius: "3px", display: "flex", justifyContent: 'center', alignItems: "center"}}>
-                        <a href={props.RightButton.to} style={{textDecoration: "none", fontWeight: "600", color: "#dba10d"}}>
-                            {props.RightButton.title}
-                        </a>
+                    <Grid item container xs={12} style={{display:"flex", justifyContent: "center", paddingTop:"10px", height: "auto"}}>
+                        
+                            <a href={props.LeftButton.to}  style={{textDecoration: "none", textAlign: "center", margin: "10px",  borderRadius: "3px", background: "#dba10d", display: "flex", justifyContent: 'center', alignItems: "center", width: "170px", height: "50px"}}>
+                                <div style={{textDecoration: "none",fontWeight: "600", color: "white"}}>
+                                    {props.LeftButton.title}
+                                </div>
+                            </a>
+                            <a href={props.RightButton.to}  style={{textDecoration: "none", textAlign: "center", margin: "10px", border: "#3E0E40 2px solid", borderRadius: "3px", display: "flex", justifyContent: 'center', alignItems: "center",  width: "170px", height: "50px"}}>
+                                <div style={{textDecoration: "none", fontWeight: "600", color: "#3E0E40"}}>
+                                {props.RightButton.title}
+                                </div>
+                            </a>
                     </Grid>
                 </Grid>
-
-            </Grid>
-            <Grid item xs={10} style={{marginBottom: "100px",}}>
-                {/* IMG */}
-                <p style={{fontWeight: "600", fontSize: "24px", paddingBottom: "6px"}} >
-                    {props.img.title}
-                </p>
-                <img src={props.img.src} width="100%" height= "auto" alt={props.img.alt}></img>
-            </Grid>
+                <Grid item xs={10} style={{marginBottom: "100px",}}>
+                    {/* IMG */}
+                    <p style={{fontWeight: "600", fontSize: "24px", paddingBottom: "6px"}} >
+                        {props.img.title}
+                    </p>
+                    <img src={props.img.src} width="100%" height= "auto" alt={props.img.alt}></img>
+                </Grid>
         </Grid>
     )
 }
